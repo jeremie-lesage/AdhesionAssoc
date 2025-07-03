a8e880ba3c80c12fe1e62bbac9ca3f2b03953186
Jérémie Lesage <jeremie.lesage@jeci.fr>
2025-07-04 00:59:48 +0200

    logo

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-page">
+    <img src="@/assets/images/logo_foyer_rural.png" alt="Logo Foyer Rural de Fauverney" class="logo">
     <h1>Bienvenue au Foyer Rural !</h1>
     <p>Nous sommes ravis de vous compter parmi nous. Pour adhérer à notre association et participer à nos activités, veuillez suivre le processus d'inscription ci-dessous.</p>
 
@@ -33,6 +34,12 @@ import { RouterLink } from 'vue-router';
 .home-page h1 {
   color: var(--color-primary);
   margin-bottom: 20px;
+}
+
+.home-page .logo {
+  max-width: 150px; /* Ajustez la taille selon vos besoins */
+  height: auto;
+  margin-bottom: 30px;
 }
 
 .home-page h2 {
